032e69693b4b7dbf507473d0b38b4c2ed96bdb0c
wunmicrown <olayiwolapamilerin34@gmail.com>
2023-12-23 15:42:17 +0100

    make a changes

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 "use strict"
 require('dotenv').config();
 const app=express();
-const Port=process.env.port||4500;
+const Port=process.env.port||5000;
 const server=http.createServer(app);
+// app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.get('/',(req,res)=>{
+    return res.status(200).json({message: 'Hello!'})
+});
