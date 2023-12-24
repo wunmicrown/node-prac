cf7a9ee98028c3516506bf4f020a864b0c0487b7
wunmicrown <olayiwolapamilerin34@gmail.com>
2023-12-24 05:02:05 +0100

    setting my express to json to send data to my clients

--- index.js
+++ index.js
@@ -4,4 +4,6 @@ const http = require('http');
 const express = require('express');
 
 const app = express();
-const port = process.env.PORT || 5000; // Use PORT instead of port
+const port = process.env.PORT || 5000; 
+
+app.use(express.json());
