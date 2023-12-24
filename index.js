1b26022c939c2ff5f60123b35be85762826019a5
wunmicrown <olayiwolapamilerin34@gmail.com>
2023-12-24 05:15:45 +0100

    maka a changes

--- index.js
+++ index.js
@@ -4,18 +4,20 @@ const http = require('http');
 const express = require('express');
 
 const app = express();
-const port = process.env.PORT || 5000; 
+const port = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
-  return res.status(200).json({ message: 'Hello!' });
+    return res.status(200).json({ message: 'Hello!' });
 
+
+
+    app.post('/send-names', (req, res) => {
+        const names = req.body.names || [];
+    })
+    console.log("Received names:", names);
     return res.status(200).json({ message: 'Names received successfully', names });
 
 });
-
-app.post('/send-names', (req, res) => {
-      const names = req.body.names || [];})
-      console.log("Received names:", names);
