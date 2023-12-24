af3fd1d363acfc737c743b822c8241fe1a32c249
wunmicrown <olayiwolapamilerin34@gmail.com>
2023-12-24 05:09:42 +0100

    sending a request to my sever

--- index.js
+++ index.js
@@ -8,3 +8,7 @@ const port = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+  return res.status(200).json({ message: 'Hello!' });
+});
